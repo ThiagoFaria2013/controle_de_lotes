804c3f74b62ec42e609a182c757db71dd08222f3
Thiago Augusto <thicofaria@outlook.com>
2023-08-22 20:07:07 -0300

    fdsfesdfse

--- controle_lotes.py
+++ controle_lotes.py
@@ -3,4 +3,4 @@ import pandas as pd
 df = pd.read_csv(r'C:\Users\Usuario\Documents\csv_teste\sicoob_2022_04_10_12_06_13-_1_.csv')
 
 print(df)
-print('teste para o github, testando mais ainda teste teste fdsafdsa')
+print('teste para o github, testando mais ainda teste test njkguojdfsfesdfesdfesbn;e fdsafdsa')
