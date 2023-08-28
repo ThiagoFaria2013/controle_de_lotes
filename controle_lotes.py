673c9bba0c99ed965a4804d2a7ab0a3f2a7944c3
Thiago Augusto <thicofaria@outlook.com>
2023-08-28 19:11:03 -0300

    apagando mensagens de teste

--- controle_lotes.py
+++ controle_lotes.py
@@ -3,4 +3,4 @@ import pandas as pd
 df = pd.read_csv(r'C:\Users\Usuario\Documents\csv_teste\sicoob_2022_04_10_12_06_13-_1_.csv')
 
 print(df)
-print('teste para o github, testando mais ainda teste test njkguojdfsfesdfesdgdfsfesfesbn;e fdsafdsa')
+print('teste para o github, testando mais ainda teste test njkggyhukfgvhjmuojdfsfesdfesdgdfsfesfesbn;e fdsafdsa')
