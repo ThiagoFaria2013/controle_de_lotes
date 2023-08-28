ba3567c232b2e4c622e1ea87070a765231fec82b
Thiago Augusto <thicofaria@outlook.com>
2023-08-28 19:37:56 -0300

    adicioando como ler as colunas

--- controle_lotes.py
+++ controle_lotes.py
@@ -1,3 +1,18 @@
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\Usuario\Documents\csv_teste\sicoob_2022_04_10_12_06_13-_1_.csv')
+linhas_validas = []
+
+
+with open(r'C:\Users\Usuario\Documents\csv_teste\sicoob_2022_04_10_12_06_13-_1_.csv', 'r') as arquivo:
+
+    for _ in range(9):
+        next(arquivo)
+
+
+    for linha in arquivo:
+        campos = linha.strip().split()
+        if len(campos) == 3:
+            linhas_validas.append(campos)
+
+
+df = pd.DataFrame(linhas_validas, columns=['DATA', 'HISTORICO', 'VALOR'])
