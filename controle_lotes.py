8ed03820f009fac33badd4122c4d8b4d9bada6fd
Thiago Augusto <thicofaria@outlook.com>
2023-08-29 19:36:37 -0300

    arquivo separado corretamente

--- controle_lotes.py
+++ controle_lotes.py
@@ -1,21 +1,21 @@
-import tabula
-import pandas as pd
+import pdfplumber
 
 pdf_file = 'C:\\Users\\Usuario\\Desktop\\trabalho\\sicoob_2022_04_10_12_06_13 (1).pdf'
 
 all_tables = []
-for page_num in range(len(tabula.read_pdf(pdf_file, pages='1'))):
-    tables = tabula.read_pdf(pdf_file, pages=str(page_num + 1))
-    if tables:
+
+with pdfplumber.open(pdf_file) as pdf:
+    for page in pdf.pages:
+        tables = page.extract_tables()
         all_tables.extend(tables)
 
 for idx, table in enumerate(all_tables):
-    table = table.iloc[8:-11]
+    # Processamento da tabela
+    table = pd.DataFrame(table[1:], columns=table[0])  # Supondo que a primeira linha seja um cabeçalho
 
     print(f"Tabela {idx + 1}:\n")
     print(table)
     print("\n")
 
-for idx, table in enumerate(tables):
-
+    # Salvar cada tabela em um arquivo CSV
     table.to_csv(f'tabela_{idx + 1}.csv', index=False)
