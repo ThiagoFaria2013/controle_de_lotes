7a6231d269ca05aa81ab78cbf47c583571c21ed9
Thiago Augusto <thicofaria@outlook.com>
2023-08-29 19:23:26 -0300

    ignorando as informacoes inuteis do pdf

--- controle_lotes.py
+++ controle_lotes.py
@@ -5,7 +5,12 @@ pdf_file = 'C:\\Users\\Usuario\\Desktop\\trabalho\\sicoob_2022_04_10_12_06_13 (1
 tables = tabula.read_pdf(pdf_file, pages='all')
 
 for idx, table in enumerate(tables):
-    # Exemplo: Imprima a tabela
+    table = table.iloc[8:-12]
+
     print(f"Tabela {idx + 1}:\n")
     print(table)
     print("\n")
+
+for idx, table in enumerate(tables):
+
+    table.to_csv(f'tabela_{idx + 1}.csv', index=False)
