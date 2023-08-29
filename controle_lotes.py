fec097388cf392453bfb5b372ee06d9515fa5864
Thiago Augusto <thicofaria@outlook.com>
2023-08-29 19:34:22 -0300

    ajuste das paginas sendo lidas

--- controle_lotes.py
+++ controle_lotes.py
@@ -1,11 +1,16 @@
 import tabula
+import pandas as pd
 
 pdf_file = 'C:\\Users\\Usuario\\Desktop\\trabalho\\sicoob_2022_04_10_12_06_13 (1).pdf'
 
-tables = tabula.read_pdf(pdf_file, pages='all')
+all_tables = []
+for page_num in range(len(tabula.read_pdf(pdf_file, pages='1'))):
+    tables = tabula.read_pdf(pdf_file, pages=str(page_num + 1))
+    if tables:
+        all_tables.extend(tables)
 
-for idx, table in enumerate(tables):
-    table = table.iloc[8:-12]
+for idx, table in enumerate(all_tables):
+    table = table.iloc[8:-11]
 
     print(f"Tabela {idx + 1}:\n")
     print(table)
